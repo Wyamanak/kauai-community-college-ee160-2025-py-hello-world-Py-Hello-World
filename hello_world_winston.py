f06e6e82fe62aad73b62bdbb2119f4d99617782d
Yamanaka Winston <wyamanak@hawaii.edu>
2025-05-11 00:34:22 -1000

    Add files via upload

---
+++ hello_world_winston.py
@@ -0,0 +1,9 @@
+﻿#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# HelloWorld.py prints the text "Hello World!" to the display.
+#
+# Created by Winston Yamanaka, 2025-05-08
+# Last edit: 2025-05-08
+# Description:it prints hello world 
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+print("Hello World!")
